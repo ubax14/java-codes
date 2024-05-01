3aaff264ed82872a71d6274fdfffdf6f95f1f077
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-05-01 11:58:57 -0700

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+    public static void main(String[] args) {
+        int count=0;
+        while (count < 10){
+            System.out.println("welcome to java");
+            count++;
+        }
+    }
+}
