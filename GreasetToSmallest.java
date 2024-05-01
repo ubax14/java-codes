d28e8b49edf5b6d8f1ed817cc99f65e4823eb823
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-05-01 11:58:17 -0700

    Add files via upload

---
+++ GreasetToSmallest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GreasetToSmallest {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("enter A Number1 :");
+        int num1 =input.nextInt();
+        System.out.print("enter A Number2 :");
+        int num2 =input.nextInt();
+        System.out.print("enter A Number3 :");
+        int num3 =input.nextInt();
+
+        if(num1>num2 && num1>num3);
+
+    }
+}
