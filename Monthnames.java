338662cd4e129474ac0cbbe65610c6cf3babe501
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-05-01 12:09:30 -0700

    Add files via upload

---
+++ Monthnames.java
@@ -0,0 +1,59 @@
+
+//import java.time.Year;
+//import java.util.Scanner;
+//
+//import java.util.Scanner;
+//public class Monthnames{
+//    public static void main(String[]args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("enter a year ");
+//        int year = input.nextInt();
+//        System.out.print("Enter the month b/w 1 and 12 :");
+//        int month = input.nextInt();
+////        boolean isleabyear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+////
+//        switch (month) {
+//            case 1:
+//                System.out.print("there is 31 days in this month january(1) ");
+//                break;
+//            case 2:
+//                System.out.println("there is 28/29 days in this month of february(2)");
+//                boolean isleabyear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+//                if (isleabyear) System.out.println("february");
+//                break;
+//            case 3:
+//                System.out.println("there is 31 days in this month march(3)");
+//                break;
+//            case 4:
+//                System.out.println("there is 30 days in this month of april(4)");
+//                break;
+//            case 5:
+//                System.out.println("there is 31 days in this month may(5)");
+//                break;
+//            case 6:
+//                System.out.println("there is 30 days in this month of june(6)");
+//                break;
+//            case 7:
+//                System.out.println("there is 31 days in this month july(7)");
+//                break;
+//            case 8:
+//                System.out.println("there is 31 days in this month august(8)");
+//                break;
+//            case 9:
+//                System.out.println("there is 30 days in this month september(9)");
+//                break;
+//            case 10:
+//                System.out.println("there is 31  days in this month october(10)");
+//                break;
+//            case 11:
+//                System.out.println("there is 30 days in this month november(11)");
+//                break;
+//            case 12:
+//                System.out.println("there is 31 days in this month month(12)");
+//                break;
+//            default:
+//                System.out.print("invalid months");
+//        }
+//
+//    }
+//}
