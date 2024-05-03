eb2dbf37c83011ef034f9f3a8889d6b1e4644763
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-05-03 01:08:35 -0700

    Add files via upload

---
+++ trignometry.java
@@ -0,0 +1,20 @@
+public class trignometry {
+    public static void main(String[] args) {
+        //Exponent methods
+        System.out.println("These are exponent methods ");
+        System.out.println(Math.pow(3,4));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.log(2.71));
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.sqrt(10.5));
+        //Rounded methods
+        System.out.println("These are rounded methods");
+        System.out.println(Math.ceil(3.7));
+        System.out.println(Math.round(3.7));
+        System.out.println(Math.rint(3.7));
+        System.out.println(Math.floor(3.7));
+        System.out.println('a'+'1');
+        System.out.println('a'+1);
+        System.out.println('a'+"ali");
+    }
+}
