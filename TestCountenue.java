9ea8aad2c6bd30a100ca62155120a6b494991bdb
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-08-03 12:42:39 -0700

    Add files via upload

---
+++ TestCountenue.java
@@ -0,0 +1,15 @@
+package Ch5;
+
+public class TestCountenue {
+    public static void main(String[] args) {
+        int sum=0;
+        int number=0;
+        while (number<20){
+            number++;
+            if (number==10 || number==11)
+                continue;
+            sum+=number;
+        }
+        System.out.println("the sum is "+sum);
+    }
+}
