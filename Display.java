1e666f259bb779de8518145de789207305c4efd2
ubax14 <139847114+ubax14@users.noreply.github.com>
2024-08-03 12:44:25 -0700

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,11 @@
+package Ch6;
+
+public class Display {
+    public static void main(String[] args) {
+        Display("welcome to java");
+    }
+    public static void Display( String message){
+
+            System.out.println(message);
+
+}}
